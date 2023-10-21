4a513424d80eb9aa1ff215616c067fb52ad0174f
IASP23 <alexanderpilataxi232002@gmail.com>
2023-10-20 21:46:02 -0500

    Creation of components for the portfolio

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+  darkMode: "class",
+  theme: {
+    colors: {
+      // Configure your color palette here
+    },
+  },
+};
